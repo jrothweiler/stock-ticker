10b9d0ce1b220900c7a1ee4c734b8518ca51931b
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-07-14 13:17:36 -0400

    remove console logs

--- server/index.js
+++ server/index.js
@@ -7,9 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/quote/:symbol', async (req, res) => {
     const symbol = req.params.symbol;
-    console.log("fetching quote")
     const quoteData = await iex.quote(symbol);
-    console.log("done fetching quote")
     res.setHeader('Content-Type', 'application/json');
     const { previousClose, week52High, week52Low, high, low, latestPrice, latestVolume, marketCap, open, avgTotalVolume } = quoteData;
     res.send(JSON.stringify({ previousClose, week52High, week52Low, high, low, latestPrice, marketCap, latestVolume, open, avgTotalVolume }));
@@ -17,9 +15,7 @@ app.get('/api/quote/:symbol', async (req, res) => {
 
 app.get('/api/stats/:symbol', async (req, res) => {
     const symbol = req.params.symbol;
-    console.log("fetching stats")
     const statData = await iex.keyStats(symbol);
-    console.log("done fetching stats")
     res.setHeader('Content-Type', 'application/json');
     const { dividendYield, ttmEPS, peRatio } = statData;
     res.send(JSON.stringify({ dividendYield, earningsPerShare: ttmEPS, peRatio}));
@@ -27,9 +23,7 @@ app.get('/api/stats/:symbol', async (req, res) => {
 
 app.get('/api/company/:symbol', async (req, res) => {
     const symbol = req.params.symbol;
-    console.log("fetching company")
     const companyData = await iex.company(symbol);
-    console.log("done fetching company")
     res.setHeader('Content-Type', 'application/json');
     const { companyName, website, description } = companyData;
     res.send(JSON.stringify({ companyName, website, description }));
@@ -37,9 +31,7 @@ app.get('/api/company/:symbol', async (req, res) => {
 
 app.get('/api/news/:symbol', async (req, res) => {
     const symbol = req.params.symbol;
-    console.log("fetching news")
     const newsData = await iex.news(symbol, 5);
-    console.log("done fetching news")
     res.setHeader('Content-Type', 'application/json');
     const returnData = newsData.map(article => {
         const {datetime, headline, source, url} = article;
