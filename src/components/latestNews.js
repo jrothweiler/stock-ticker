74d0e7335caf622689142e9b8a39c02e9929eec6
Mark Bekker <mb6396@nyu.edu>
2020-07-17 09:42:02 -0400

    small changes

--- src/components/latestNews.js
+++ src/components/latestNews.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { newsSelector } from "../selectors/newsSelector";
-import { companySelector} from "../selectors/companySelector";
-import { statsSelector } from "../selectors/statsSelector";
-import { quoteSelector } from "../selectors/quoteSelector";
 import { DisplayWrapper } from "./generics/displayWrapper";
 import { TitleHeader } from "./generics/titleHeader";
 import { Text } from "./generics/text";
-import * as timeago from 'timeago.js';
+import * as timeago from "timeago.js";
 import { useDispatch, useSelector } from "react-redux";
 
 export const LatestNews = () => {
   //Call necessary selectors for display data
   const dispatch = useDispatch();
-    const newsInfo = useSelector(newsSelector);
+  const newsInfo = useSelector(newsSelector);
 
   return (
     <DisplayWrapper width="25%" height="25%">
       <TitleHeader>Latest News</TitleHeader>
       <DisplayWrapper overflow="auto" height="20.0rem">
-      {newsInfo && newsInfo.map((article) => 
-        <div key={article.headline} className="newsRow">
-          <Text variant="primary">{article.headline}</Text>
-          <Text mt="0.3rem" variant="secondary">{timeago.format(article.datetime)} - {article.source}</Text>
-        </div>)
-      }
+        {newsInfo &&
+          newsInfo.map((article) => (
+            <div key={article.headline} className="newsRow">
+              <Text variant="primary">{article.headline}</Text>
+              <Text mt="0.3rem" variant="secondary">
+                {timeago.format(article.datetime)} - {article.source}
+              </Text>
+            </div>
+          ))}
       </DisplayWrapper>
     </DisplayWrapper>
   );
