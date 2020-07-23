0672a59c5db4db8e720236eb54fedd6866fadffa
Mark Bekker <mark.bekker@weareadaptive.com>
2020-07-23 13:47:19 -0400

    Fixed styling issue with title header

--- src/components/generics/titleHeader.js
+++ src/components/generics/titleHeader.js
@@ -12,12 +12,7 @@ export const TitleHeader = styled("div")(
         borderBottom: "0.2rem solid #6491d3",
         fontSize: "16px",
         fontWeight: "bold",
-        color: "#7fb3ff",
         fontFamily: "Lato",
-        fontStretch: "normal",
-        fontStyle: "normal",
-        lineHeight: "normal",
-        letterSpacing: "normal",
         textAlign: "left",
         color: "#7fb3ff"
       },
