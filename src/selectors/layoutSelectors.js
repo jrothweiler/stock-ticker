986c08600545ffc387bf8f84a7bf290f91dd0199
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-07-27 15:48:31 -0400

    Render different layout bodys depending on width

--- src/selectors/layoutSelectors.js
+++ src/selectors/layoutSelectors.js
@@ -2,11 +2,17 @@ import { createSelector } from 'reselect';
 
 const selectLayoutData = (state) => state.layout;
 
-export const selectIsMobileLayout = createSelector([selectLayoutData], (layout) => !layout.isDesktopSize && !layout.isTabletSize);
+export const selectIsMobileLayout = createSelector([selectLayoutData], (layout) => { 
+    return !layout.isDesktopSize && !layout.isTabletSize; 
+});
 
-export const selectIsTabletLayout = createSelector([selectLayoutData], (layout) => !layout.isDesktopSize && layout.isTabletSize);
+export const selectIsTabletLayout = createSelector([selectLayoutData], (layout) => { 
+    return !layout.isDesktopSize && layout.isTabletSize 
+});
 
-export const selectIsDesktopLayout = createSelector([selectLayoutData], (layout) => layout.isDesktopSize && !layout.isTabletSize);
+export const selectIsDesktopLayout = createSelector([selectLayoutData], (layout) => {
+    return layout.isDesktopSize;
+});
 
 export const selectLayout = createSelector([selectIsDesktopLayout, selectIsTabletLayout], (desktop, tablet) => {
     return desktop ? 'desktop' : tablet ? 'tablet' : 'mobile'
--- src/stockTrader.js
+++ src/stockTrader.js
@@ -7,11 +7,34 @@ import { SearchBar } from "./components/searchBar";
 import { Header } from "./components/header";
 import { VisualDisplay } from "./components/visualDisplay";
 import { PriceDisplay } from "./components/priceDisplay";
-import { Text } from "./components/generics/text";
-import { TitleHeader } from "./components/generics/titleHeader";
-import { useDispatch } from "react-redux";
+import { selectLayout } from "./selectors/layoutSelectors";
+import { useSelector } from "react-redux";
 
 export const StockTrader = () => {
+  const layout = useSelector(selectLayout);
+
+  console.log(layout);
+
+  const bodyLayout = layout === "desktop" ? (
+    <DisplayWrapper variant="flexRow">
+      <DisplayWrapper width="65%" variant="flexColumn">
+        <VisualDisplay height="55%" />
+        <KeyStats height="35%" />
+      </DisplayWrapper>
+      <DisplayWrapper width="27.5%" variant="flexColumn">
+        <LatestNews height="50%" />
+        <CompanyOverview height="35%" />
+      </DisplayWrapper>
+    </DisplayWrapper>
+  ) : (
+    <DisplayWrapper variant="flexColumn" width="100%">
+        <VisualDisplay height="40%" />
+        <KeyStats height="25%" />
+        <LatestNews height="20%" />
+        <CompanyOverview height="15%" />
+    </DisplayWrapper>
+  );
+
   return (
     <DisplayWrapper ml="5%" mr="5%" mt="2.5%" mb="5%" max-width="100%">
       <DisplayWrapper variant="flexRow" mb="2.0rem">
@@ -27,16 +50,7 @@ export const StockTrader = () => {
         <SearchBar />
         <PriceDisplay />
       </DisplayWrapper>
-      <DisplayWrapper variant="flexRow">
-        <DisplayWrapper width="65%" variant="flexColumn">
-          <VisualDisplay height="55%" />
-          <KeyStats height="35%" />
-        </DisplayWrapper>
-        <DisplayWrapper width="27.5%" variant="flexColumn">
-          <LatestNews height="50%" />
-          <CompanyOverview height="35%" />
-        </DisplayWrapper>
-      </DisplayWrapper>
+      {bodyLayout}
     </DisplayWrapper>
   );
 };
