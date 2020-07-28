67bf282bfef763a67dede518d9b134ad19bbe01d
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-07-28 13:11:59 -0400

    go to one column key stats on mobile

--- src/components/keyStats.js
+++ src/components/keyStats.js
@@ -15,88 +15,91 @@ export const KeyStats = (props) => {
   return (
     <DisplayWrapper height={props.height}>
       <TitleHeader>KEY STATS</TitleHeader>
-      <DisplayWrapper display="inline-block" width="46%" mr="4%">
-        {quote && (
-          <StatWrapper>
-            <Text variant="statLabel">Previous Close</Text>
-            <Text variant="statValue">
-              {quote.previousClose.toLocaleString("en")}
-            </Text>
-          </StatWrapper>
-        )}
-        {quote && (
-          <StatWrapper>
-            <Text variant="statLabel">Day Range</Text>
-            <Text variant="statValue">
-              {quote.low.toLocaleString("en")} - {quote.high.toLocaleString("en")}
-            </Text>
-          </StatWrapper>
-        )}
-        {quote && (
-          <StatWrapper>
-            <Text variant="statLabel">Volume</Text>
-            <Text variant="statValue">{quote.latestVolume.toLocaleString("en")}</Text>
-          </StatWrapper>
-        )}
-        {quote && (
-          <StatWrapper>
-            <Text variant="statLabel">Market Cap</Text>
-            <Text variant="statValue">
-              {quote.marketCap.toLocaleString("en")}
-            </Text>
-          </StatWrapper>
-        )}
-        {stats && (
-          <StatWrapper>
-            <Text variant="statLabel">P/E Ratio</Text>
-            <Text variant="statValue">
-              {stats.peRatio.toLocaleString("en")}
-            </Text>
-          </StatWrapper>
-        )}
+      <DisplayWrapper variant="flexRow">
+        <DisplayWrapper width={["100%", "46%"]}>
+          {quote && (
+            <StatWrapper>
+              <Text variant="statLabel">Previous Close</Text>
+              <Text variant="statValue">
+                {quote.previousClose.toLocaleString("en")}
+              </Text>
+            </StatWrapper>
+          )}
+          {quote && (
+            <StatWrapper>
+              <Text variant="statLabel">Day Range</Text>
+              <Text variant="statValue">
+                {quote.low.toLocaleString("en")} - {quote.high.toLocaleString("en")}
+              </Text>
+            </StatWrapper>
+          )}
+          {quote && (
+            <StatWrapper>
+              <Text variant="statLabel">Volume</Text>
+              <Text variant="statValue">{quote.latestVolume.toLocaleString("en")}</Text>
+            </StatWrapper>
+          )}
+          {quote && (
+            <StatWrapper>
+              <Text variant="statLabel">Market Cap</Text>
+              <Text variant="statValue">
+                {quote.marketCap.toLocaleString("en")}
+              </Text>
+            </StatWrapper>
+          )}
+          {stats && (
+            <StatWrapper>
+              <Text variant="statLabel">P/E Ratio</Text>
+              <Text variant="statValue">
+                {stats.peRatio.toLocaleString("en")}
+              </Text>
+            </StatWrapper>
+          )}
+        </DisplayWrapper>
+        <DisplayWrapper width={["100%", "46%"]}>
+          {quote && (
+            <StatWrapper>
+              <Text variant="statLabel">Open</Text>
+              <Text variant="statValue">{quote.open.toLocaleString("en")}</Text>
+            </StatWrapper>
+          )}
+          {quote && (
+            <StatWrapper>
+              <Text variant="statLabel">52 Week Range</Text>
+              <Text variant="statValue">
+                {" "}
+                {quote.week52Low.toLocaleString("en")} -{" "}
+                {quote.week52High.toLocaleString("en")}
+              </Text>
+            </StatWrapper>
+          )}
+          {quote && (
+            <StatWrapper>
+              <Text variant="statLabel">Total Avg Volume</Text>
+              <Text variant="statValue">
+                {quote.avgTotalVolume.toLocaleString("en")}
+              </Text>
+            </StatWrapper>
+          )}
+          {stats && (
+            <StatWrapper>
+              <Text variant="statLabel">Earnings Per Share</Text>
+              <Text variant="statValue">
+                {stats.earningsPerShare.toLocaleString("en")}
+              </Text>
+            </StatWrapper>
+          )}
+          {stats && (
+            <StatWrapper>
+              <Text variant="statLabel">Dividend & Yield</Text>
+              <Text variant="statValue">
+                {stats.dividendYield ? `${(stats.dividendYield * 100).toLocaleString("en")}%` : "N/A"}
+              </Text>
+            </StatWrapper>
+          )}
+        </DisplayWrapper>
       </DisplayWrapper>
-      <DisplayWrapper display="inline-block" width="46%" ml="4%">
-        {quote && (
-          <StatWrapper>
-            <Text variant="statLabel">Open</Text>
-            <Text variant="statValue">{quote.open.toLocaleString("en")}</Text>
-          </StatWrapper>
-        )}
-        {quote && (
-          <StatWrapper>
-            <Text variant="statLabel">52 Week Range</Text>
-            <Text variant="statValue">
-              {" "}
-              {quote.week52Low.toLocaleString("en")} -{" "}
-              {quote.week52High.toLocaleString("en")}
-            </Text>
-          </StatWrapper>
-        )}
-        {quote && (
-          <StatWrapper>
-            <Text variant="statLabel">Total Avg Volume</Text>
-            <Text variant="statValue">
-              {quote.avgTotalVolume.toLocaleString("en")}
-            </Text>
-          </StatWrapper>
-        )}
-        {stats && (
-          <StatWrapper>
-            <Text variant="statLabel">Earnings Per Share</Text>
-            <Text variant="statValue">
-              {stats.earningsPerShare.toLocaleString("en")}
-            </Text>
-          </StatWrapper>
-        )}
-        {stats && (
-          <StatWrapper>
-            <Text variant="statLabel">Dividend & Yield</Text>
-            <Text variant="statValue">
-              {stats.dividendYield ? `${(stats.dividendYield * 100).toLocaleString("en")}%` : "N/A"}
-            </Text>
-          </StatWrapper>
-        )}
-      </DisplayWrapper>
+      
     </DisplayWrapper>
   );
 };
