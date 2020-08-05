46c54f365c237fe7ed9d5e7547409ae1464c6084
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-08-05 18:11:55 -0400

    More cleanup

--- src/components/priceDisplay.js
+++ src/components/priceDisplay.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Text } from "./generics/text";
 import { mdiArrowDown, mdiArrowUp } from "@mdi/js";
 import Icon from "@mdi/react";
-import { useDispatch, useSelector } from "react-redux";
+import { DisplayWrapper } from "./generics/displayWrapper";
 
 export const PriceDisplay = (props) => {
   return (
-    <div>
+    <DisplayWrapper {...props}>
       {props.data && (
         <div>
           <Text
@@ -24,7 +24,11 @@ export const PriceDisplay = (props) => {
             size={props.size}
             display="inline-block"
           >
-            <Text fontSize="20px" verticalAlign="top" display="inline-block">
+            <Text
+              fontSize={props.size === "medium" ? "8px" : "20px"}
+              verticalAlign="top"
+              display="inline-block"
+            >
               $
             </Text>
             {props.data.latestPrice.toLocaleString("en")} &nbsp;
@@ -79,12 +83,16 @@ export const PriceDisplay = (props) => {
                 (props.data.latestPrice - props.data.open) / props.data.open
               ) * 100
             ).toFixed(2)}
-            <Text fontSize="20px" verticalAlign="top" display="inline-block">
+            <Text
+              fontSize={props.size === "medium" ? "8px" : "20px"}
+              verticalAlign="top"
+              display="inline-block"
+            >
               %
             </Text>
           </Text>
         </div>
       )}
-    </div>
+    </DisplayWrapper>
   );
 };
