0e4646f5a8f7f4770dd203fd43ba6a5d9e1896cf
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-08-06 14:00:02 -0400

    Truncate suggestions and center market open text

--- src/components/marketInfo.js
+++ src/components/marketInfo.js
@@ -18,6 +18,8 @@ export const MarketInfo = () => {
       <Text
         variant="secondary"
         size="small"
+        height="1.5rem"
+        lineHeight="1.5rem"
         mr="16px"
         display={["none", "none", "inline-block"]}
       >
@@ -25,10 +27,16 @@ export const MarketInfo = () => {
       </Text>
       <Icon
         path={marketOpen ? mdiWeatherSunny : mdiWeatherNight}
-        size={1.0}
+        size="1.5rem"
         color="#ffbb5e"
       />
-      <Text variant="primary" size="small" ml="4px">
+      <Text
+        variant="primary"
+        size="small"
+        ml="4px"
+        height="1.5rem"
+        lineHeight="1.5rem"
+      >
         Market {marketOpen ? "Open" : "Closed"}
       </Text>
     </DisplayWrapper>
