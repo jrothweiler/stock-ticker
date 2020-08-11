1767676456e050099d55369d1efda54ff2275a3a
Mark-Adaptive <mark.bekker@weareadaptive.com>
2020-08-11 18:24:01 -0400

    Beginning typescript conversion on footer

--- src/stockTrader.js
+++ src/stockTrader.js
@@ -26,50 +26,50 @@ export const StockTrader = () => {
     <DisplayWrapper mt="2.5%" mb="5%" max-width="100%">
       {ticker && (
         <>
-        <DisplayWrapper ml="5%" mr="5%" max-width="100%">
-          <DisplayWrapper variant="flexRow" mb="2.0rem">
-            <Header />
-          </DisplayWrapper>
-          <DisplayWrapper
-            variant={["flexColumn", "flexRow", "flexRow"]}
-            paddingBottom="16px"
-            borderBottom="0.2rem solid #6491d3"
-            width="100%"
-          >
-            <SearchBar
-              width={["100%", "30%", "50%"]}
-              mb={["8px", "0px", "0px"]}
-            />
-            <PriceDisplay
-              data={quote}
-              size={["mediumLarge", "large", "large"]}
-            />
-          </DisplayWrapper>
-          <DisplayWrapper variant="flexRow" mt="16px" mb="24px">
-            <CompanyBadges />
-            <MarketInfo />
-          </DisplayWrapper>
-          <DisplayWrapper variant="flexColumn">
-            <DisplayWrapper height="50%" variant="flexRow">
-              <VisualDisplay width={["100%", null, "64%"]} mb="32px" />
-              <LatestNews width={["100%", null, "33%"]} mb="32px" />
+          <DisplayWrapper ml="5%" mr="5%" max-width="100%">
+            <DisplayWrapper variant="flexRow" mb="2.0rem">
+              <Header />
             </DisplayWrapper>
-            <DisplayWrapper height="35%" variant="flexRow">
-              <KeyStats width={["100%", null, "64%"]} mb="32px" />
+            <DisplayWrapper
+              variant={["flexColumn", "flexRow", "flexRow"]}
+              paddingBottom="16px"
+              borderBottom="0.2rem solid #6491d3"
+              width="100%"
+            >
+              <SearchBar
+                width={["100%", "30%", "50%"]}
+                mb={["8px", "0px", "0px"]}
+              />
+              <PriceDisplay
+                data={quote}
+                size={["mediumLarge", "large", "large"]}
+              />
+            </DisplayWrapper>
+            <DisplayWrapper variant="flexRow" mt="16px" mb="24px">
+              <CompanyBadges />
+              <MarketInfo />
+            </DisplayWrapper>
+            <DisplayWrapper variant="flexColumn">
+              <DisplayWrapper height="50%" variant="flexRow">
+                <VisualDisplay width={["100%", null, "64%"]} mb="32px" />
+                <LatestNews width={["100%", null, "33%"]} mb="32px" />
+              </DisplayWrapper>
+              <DisplayWrapper height="35%" variant="flexRow">
+                <KeyStats width={["100%", null, "64%"]} mb="32px" />
 
-              <DisplayWrapper
-                width={["100%", null, "33%"]}
-                variant="flexColumn"
-              >
-                <CompanyOverview mb="32px" />
-                <TopPeers />
+                <DisplayWrapper
+                  width={["100%", null, "33%"]}
+                  variant="flexColumn"
+                >
+                  <CompanyOverview mb="32px" />
+                  <TopPeers />
+                </DisplayWrapper>
               </DisplayWrapper>
             </DisplayWrapper>
           </DisplayWrapper>
-        </DisplayWrapper>
-        <DisplayWrapper mt="2.0rem" width="100%">
-        <Footer width="100%" height="5%" />
-      </DisplayWrapper>
+          <DisplayWrapper mt="2.0rem" width="100%">
+            <Footer width="100%" height="5%" />
+          </DisplayWrapper>
         </>
       )}
     </DisplayWrapper>
