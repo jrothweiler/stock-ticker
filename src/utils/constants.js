25e5101272e60f2f136b148598d2dac7b30d3e75
Mark Bekker <mark.bekker@weareadaptive.com>
2020-08-12 16:18:23 -0400

    changes

--- src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const INITIAL_STOCK = "AAPL";
-export const VALID_SEARCH_REGEXP = /^[A-Za-z]+$/;
-export const INITIAL_CHART_RANGE = '1D';
-export const POSSIBLE_CHART_RANGES = ['1D', '5D', '1M', '1Y', '5Y', 'MAX']
