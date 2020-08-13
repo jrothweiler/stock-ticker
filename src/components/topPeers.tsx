f56b6f946ae353e3b5591d1e8fce334145cfdb52
Justin Rothweiler <justin.rothweiler@weareadaptive.com>
2020-08-13 09:50:07 -0400

    Peers component

---
+++ src/components/topPeers.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { DisplayWrapper } from "./generics/displayWrapper";
+import { TitleHeader } from "./generics/titleHeader";
+import { Text } from "./generics/text";
+
+import { usePeersSelector } from "./componentHooks/usePeersSelector";
+import { useSearchSymbol } from "./componentHooks/useSearchSymbol";
+import type { StyleProps } from "../types/styleTypes";
+
+export const TopPeers = (props: StyleProps) => {
+  const peersInfo = usePeersSelector();
+  const searchSymbol = useSearchSymbol();
+
+  return (
+    peersInfo && (
+      <DisplayWrapper {...props}>
+        <TitleHeader>TOP PEERS</TitleHeader>
+        {peersInfo.map((peer) => (
+          <Text
+            key={peer}
+            className="topPeer"
+            mr="18px"
+            display="inline-block"
+            variant="secondary"
+            opacity={1}
+            onClick={() => searchSymbol(peer)}
+          >
+            {peer}
+          </Text>
+        ))}
+        {/* WORK is added here as a valid default stock for demonstration purposes */}
+        <Text
+          className="topPeer"
+          display="inline-block"
+          variant="secondary"
+          opacity={1}
+          onClick={() => searchSymbol("WORK")}
+        >
+          WORK
+        </Text>
+      </DisplayWrapper>
+    )
+  );
+};
